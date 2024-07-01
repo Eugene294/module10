1ee7cd03207d448fa7ea560951cb754f384977f7
Eugene294 <koblia.eo@gmail.com>
2024-07-01 15:17:47 +1000

    -

--- final2/logger.cs
+++ final2/logger.cs
@@ -17,12 +17,16 @@ namespace final2
     {
         public void Error(string message)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(message);
+            Console.ResetColor();
         }
 
         public void Event(string message)
         {
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine(message);
+            Console.ResetColor();
         }
     }
 
